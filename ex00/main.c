2eeae3538217a15f6cb1b878ce53623a31e5b749
minskim2 <nicek789@naver.com>
2021-04-03 22:47:00 +0900

    a

--- ex00/main.c
+++ ex00/main.c
@@ -6,47 +6,59 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 20:00:14 by minskim2          #+#    #+#             */
-/*   Updated: 2021/04/02 21:44:33 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/04/03 22:43:11 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <unistd.h>
 
 char	g_cnt[16];
-char	box[5][5];
-void	in_the_box(char *cnt);
+char	**box;
+char	in_the_box(char *box);
+
+void	error(void)
+{
+	write(1, "Error\n", 6);
+}
 
 int	is_select(char *nbr)
 {
 	int i;
 
-	i = 0;
+	i = 0;;
 	while(*nbr && i < 16)
 	{
-		if (*nbr != ' ')
+		if (*nbr >= '0' && *nbr <= '9')
 		{
 			g_cnt[i] = *nbr;
 			i++;
 		}
+		else if (!(*nbr == ' '))
+			return (0);
 		nbr++;
+
 	}
 	if (i != 16)
-	{
-		write(1, "Error\n", 6);
 		return (0);
-	}
 	return (1);
 }
 
 int	main(int n, char* str[])
 {
-	if (i == 2)
-	{
-		if(!is_select(str[i]))
-			return (0);
-		in_the_box(box, g_cnt);
-	}
-	else
-		write(1, "Error\n", 6);
+	int i;
+	int j;
+	
+	i = 0;
+	box = (char **)malloc(sizeof(char*) * 4);
+	while (i < 4)
+		box[i++] = (char *)malloc(sizeof(char) * 4);
+	if (n == 2 && is_select(str[1]))
+		in_the_box(box);
+	error();
+	i = 0;
+	while (i < 4)
+		free(box[i++]);
+	free(box);
 	return (0);
 }
